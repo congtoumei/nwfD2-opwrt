efa414eff2e35a1cb208158a9e4bbf2147e3d527
congtoumei <57308031+congtoumei@users.noreply.github.com>
2021-05-18 14:20:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,7 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git clone https://github.com/kenzok8/adguardhome.git package/adguardhome
+git clone https://github.com/kenzok8/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/kenzok8/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/kenzok8/luci-app-ssr-plus.git package/luci-app-ssr-plus
